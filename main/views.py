cf5833aff12f8fdb347aac38fd221a9540535b6b
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-12-16 14:18:03 +0500

    ➕ Added basic_vektor items

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from .forms import MatrisItemInputForm
-from .utils import krilov_method
+from .forms import MatrisItemInputForm, BasicVektorInputForm
+from .utils import krilov_method, basic_vektor
 # Create your views here.
 
 
@@ -9,8 +9,15 @@ def input_view(request):
     content = {}
     if request.method == "POST":
         null_form = MatrisItemInputForm()
+        basic_vector_form = BasicVektorInputForm()
         form = MatrisItemInputForm(request.POST)
-        if form.is_valid():
+        basic_form = BasicVektorInputForm(request.POST)
+        if form.is_valid() and basic_form.is_valid():
+            basic_item_0 = basic_form.cleaned_data['basic_item_0']
+            basic_item_1 = basic_form.cleaned_data['basic_item_1']
+            basic_item_2 = basic_form.cleaned_data['basic_item_2']
+            basic_item_3 = basic_form.cleaned_data['basic_item_3']
+
             item_00 = form.cleaned_data['item_00']
             item_01 = form.cleaned_data['item_01']
             item_02 = form.cleaned_data['item_02']
@@ -33,8 +40,20 @@ def input_view(request):
                 [int(item_20), int(item_21), int(item_22), int(item_23)],
                 [int(item_30), int(item_31), int(item_32), int(item_33)]
             ]
-            y_matris_1, y_matris_2, y_matris_3, y_matris_4, solution = krilov_method(
-                matris)
+            basic_vektor_num = basic_vektor(
+                basic_item_0, basic_item_1, basic_item_2, basic_item_3)
+            if basic_vektor_num:
+                y_matris_1, y_matris_2, y_matris_3, y_matris_4, solution = krilov_method(
+                    matris, basic_vektor_num)
+            else:
+                content = {
+                    'basic_vektor_error': "Error",
+                    "basic_vektor": basic_vektor_num,
+                    "form": form,
+                    "basic_form": basic_form,
+                }
+                return render(request, "input_request.html", content)
+
             try:
                 p1 = int(solution[0])
                 p2 = int(solution[1])
@@ -42,12 +61,6 @@ def input_view(request):
                 p4 = int(solution[3])
 
                 solution_items = f"$$D(λ) = λ^4 {'+' + str(p1) if p1 >= 0 else p1}λ^3 {'+' + str(p2) if p2 > 0 else p2}λ^2 {'+' + str(p3) if p3 > 0 else p3}λ {'+' + str(p4) if p4 > 0 else p4}$$"
-                # solution_items = {
-                #     "p1": int(solution[0]),
-                #     "p2": int(solution[1]),
-                #     "p3": int(solution[2]),
-                #     "p4": int(solution[3]),
-                # }
             except Exception as e:
                 print(e)
                 solution_items = "Error"
@@ -58,14 +71,12 @@ def input_view(request):
                 "y_matris_3": y_matris_3,
                 "y_matris_4": y_matris_4,
                 "solution": solution_items,
-                "form": null_form,
-                "matris": matris
+                "form": form,
+                "basic_form": basic_form,
             }
-
-            print("="*20)
-            print(content)
             return render(request, "input_request.html", content)
     else:
         form = MatrisItemInputForm()
-        content = {"form": form}
+        basic_form = BasicVektorInputForm()
+        content = {"form": form, "basic_form": basic_form}
         return render(request, "input_request.html", content)
